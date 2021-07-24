8c13b5e13e9ce0d4fccae6cef17c5ecf2d98b7da
Chase Cook <chasecook411@gmail.com>
2021-07-24 16:48:41 -0500

    app-def, singular names

--- src/routes/routeDefinition.ts
+++ src/routes/routeDefinition.ts
@@ -7,15 +7,15 @@ class Routes {
     static setRoutes(app: Express) {
         app.get('/status', StatusApi.get);
 
-        app.get('/runbooks/:id', RunbookApi.get);
-        app.post('/runbooks', RunbookApi.create);
-        app.put('/runbooks/:id', RunbookApi.update);
-        app.delete('/runbooks/:id', RunbookApi.delete);
+        app.get('/runbook/:id', RunbookApi.get);
+        app.post('/runbook', RunbookApi.create);
+        app.put('/runbook/:id', RunbookApi.update);
+        app.delete('/runbook/:id', RunbookApi.delete);
 
-        app.get('/users/:id', UserApi.get);
-        app.post('/users', UserApi.create);
-        app.put('/users/:id', UserApi.update);
-        app.delete('/users/:id', UserApi.delete);
+        app.get('/user/:id', UserApi.get);
+        app.post('/user', UserApi.create);
+        app.put('/user/:id', UserApi.update);
+        app.delete('/user/:id', UserApi.delete);
     }
 }
 
