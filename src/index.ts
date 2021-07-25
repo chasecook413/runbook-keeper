4a0b0ec08fd1ed0ab0661ba511bb62821e50dad7
Chase Cook <chasecook411@gmail.com>
2021-07-24 20:02:37 -0500

    error handling, deps

--- src/index.ts
+++ src/index.ts
@@ -3,21 +3,17 @@ import {connect} from 'mongoose';
 import logger from "./util/logger";
 import Routes from "./routes/routeDefinition";
 
-
 async function main() {
     const app = express();
     const API_PORT = process.env.API_PORT || 3000;
-
     const MONGO_CONNECT_URL = process.env.MONGO_CONNECT_URL || 'mongodb://mongo:27017';
-    try {
-        await connect(MONGO_CONNECT_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-    } catch (mongoConnectError) {
-        logger.emerg(`Error connecting to MongoDB: ${mongoConnectError.stack}`);
-        throw mongoConnectError;
-    }
+    const REDIS_CONNECT_HOST = process.env.REDIS_CONNECT_HOST || 'tcp://redis';
+    const REDIS_CONNECT_PORT = process.env.REDIS_CONNECT_PORT ? parseInt(process.env.REDIS_CONNECT_PORT) : 6379;
+
+    await connect(MONGO_CONNECT_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
 
     Routes.setRoutes(app);
     app.listen(API_PORT, () => {
@@ -25,4 +21,7 @@ async function main() {
     });
 }
 
-main();
+main().catch((serverError: Error) => {
+    logger.emerg(`Fatal error: ${serverError.stack}`);
+    throw serverError;
+});
